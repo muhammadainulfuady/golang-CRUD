b416b5a0037af612f256c78166060508022e2b5d
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-08-15 11:58:26 +0700

    feat: fitur crud sudah selesai pada bagian service

--- model/web/book.go
+++ model/web/book.go
@@ -1,9 +1,9 @@
 package web
 
 type CreateBookRequest struct {
-	Name        string `json:"name"`
-	Author      string `json:"author"`
-	Publication string `json:"publication"`
+	Name        string `json:"name" validate:"required, min:4, max:150"`
+	Author      string `json:"author" validate:"required, min:4, max:100"`
+	Publication string `json:"publication" validate:"required, datetime:2026-07-15"`
 }
 
 type UpdateBookRequest struct {
@@ -21,8 +21,8 @@ type BookResponse struct {
 }
 
 type ApiResponse struct {
-	Code    int         `json:"code"`
-	Status  string      `json:"status"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
+	Code    int    `json:"code"`
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Data    any    `json:"data"`
 }
--- service/book_service_impl.go
+++ service/book_service_impl.go
@@ -1,1 +1,126 @@
 package service
+
+import (
+	"context"
+
+	"golang_crud/model/domain"
+	"golang_crud/model/web"
+	"golang_crud/repository"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type BookServiceImpl struct {
+	Repository repository.BookRepository
+	Validate   *validator.Validate
+}
+
+func NewBookService(repository repository.BookRepository, validate *validator.Validate) BookService {
+	return &BookServiceImpl{
+		Repository: repository,
+		Validate:   validate,
+	}
+}
+
+func (service *BookServiceImpl) Save(ctx context.Context, request web.CreateBookRequest) (web.BookResponse, error) {
+	err := service.Validate.Struct(request)
+	if err != nil {
+		return web.BookResponse{}, err
+	}
+
+	bookDomain := domain.Book{
+		Name:        request.Name,
+		Author:      request.Author,
+		Publication: request.Publication,
+	}
+	book, err := service.Repository.Save(ctx, bookDomain)
+	if err != nil {
+		return web.BookResponse{}, err
+	}
+
+	return web.BookResponse{
+		IdBook:      book.IdBook,
+		Name:        book.Name,
+		Author:      book.Author,
+		Publication: book.Publication,
+	}, nil
+}
+
+func (service *BookServiceImpl) Update(ctx context.Context, request web.UpdateBookRequest) (web.BookResponse, error) {
+	err := service.Validate.Struct(request)
+	if err != nil {
+		return web.BookResponse{}, err
+	}
+
+	_, err = service.Repository.FindById(ctx, request.IdBook)
+	if err != nil {
+		return web.BookResponse{}, err
+	}
+
+	bookDomain := domain.Book{
+		IdBook:      request.IdBook,
+		Name:        request.Name,
+		Author:      request.Author,
+		Publication: request.Publication,
+	}
+	book, err := service.Repository.Update(ctx, bookDomain)
+	if err != nil {
+		return web.BookResponse{}, err
+	}
+
+	response := web.BookResponse{
+		IdBook:      book.IdBook,
+		Name:        book.Name,
+		Author:      book.Author,
+		Publication: book.Publication,
+	}
+	return response, nil
+}
+
+func (service *BookServiceImpl) Delete(ctx context.Context, bookId int) error {
+	_, err := service.Repository.FindById(ctx, bookId)
+	if err != nil {
+		return err
+	}
+
+	err = service.Repository.Delete(ctx, bookId)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (service *BookServiceImpl) FindById(ctx context.Context, bookId int) (web.BookResponse, error) {
+	book, err := service.Repository.FindById(ctx, bookId)
+	if err != nil {
+		return web.BookResponse{}, err
+	}
+
+	res := web.BookResponse{
+		IdBook:      book.IdBook,
+		Name:        book.Name,
+		Author:      book.Author,
+		Publication: book.Publication,
+	}
+	return res, nil
+}
+
+func (service *BookServiceImpl) FindAll(ctx context.Context) ([]web.BookResponse, error) {
+	books, err := service.Repository.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	bookResponse := []web.BookResponse{}
+	for _, book := range books {
+		res := web.BookResponse{
+			IdBook:      book.IdBook,
+			Name:        book.Name,
+			Author:      book.Author,
+			Publication: book.Publication,
+		}
+		bookResponse = append(bookResponse, res)
+	}
+	return bookResponse, nil
+}
